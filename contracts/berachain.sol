4817bd98875be302d020a9727ee087eb4c577cec
Orion1125 <oyelolaayobami12345@gmail.com>
2025-02-22 12:12:55 +0100

    first commit

---
+++ contracts/berachain.sol
@@ -0,0 +1,20 @@
+{
+  "name": "contract-deploy-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "@openzeppelin/contracts": "^5.0.1",
+    "dotenv": "^16.4.7",
+    "ethers": "^6.13.4"
+  },
+  "devDependencies": {
+    "hardhat": "^2.22.17"
+  },
+  "description": ""
+}
